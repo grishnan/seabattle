a1f2b1216d01e8b463a642f15a333eae09d50a76
grishnan <grishnan@gmail.com>
2017-11-20 21:08:57 +0300

    Create class Field

--- mw.py
+++ mw.py
@@ -2,25 +2,28 @@ import os
 import cocos as c
 from const import *
 
+class Field(c.layer.Layer):
+    ''' Field layer '''
+    def __init__(self, pic, pos):
+        super(Field, self).__init__()
+
+        field = c.sprite.Sprite(os.path.join(SD, pic), position = pos)
+        self.add(field)
+
 class MainWindow(c.layer.ColorLayer):
     ''' Main program window  '''
 
     def __init__(self, BG):
         super(MainWindow, self).__init__(*BG)
     
-        # my field
-        mf = c.sprite.Sprite(os.path.join(SD, FPIC), position = (SZ[0]//4, SZ[1]//2))
-        # enemy field
-        ef = c.sprite.Sprite(os.path.join(SD, FPIC), position = (3*SZ[0]//4, SZ[1]//2))
-
-        self.add(mf)
-        self.add(ef)
-
 def main():
     c.director.director.init(*SZ, caption = CP)
 
     wdw = MainWindow(BG)
-    scn = c.scene.Scene(wdw)
+    mf = Field(FPIC, (SZ[0]//4, SZ[1]//2))
+    ef = Field(FPIC, (3*SZ[0]//4, SZ[1]//2))
+
+    scn = c.scene.Scene(wdw, mf, ef)
 
     c.director.director.run(scn)
 
