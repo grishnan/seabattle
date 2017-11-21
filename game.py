25d169ab720c739df876266b729bc94bd8b6f24b
grishnan <grishnan@gmail.com>
2017-11-21 09:23:04 +0300

    Add testing mouse handler to Background class

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
-import os 
+import os.path 
 import cocos as c
 from const import *
 
 class Field(c.layer.Layer):
     ''' Field layer '''
+    
     def __init__(self, pic, pos):
         super(Field, self).__init__()
 
@@ -13,8 +14,18 @@ class Field(c.layer.Layer):
 class Background(c.layer.ColorLayer):
     ''' Background layer  '''
 
+    # the event handlers of this layer will be registered
+    is_event_handler = True 
+
     def __init__(self, BG):
         super(Background, self).__init__(*BG)
+
+        self.posx, self.posy = 0, 0 # inital mouse coordinates
+
+    def on_mouse_press(self, x, y, buttons, modifiers):
+        ''' Mouse handler '''
+        self.posx, self.posy = c.director.director.get_virtual_coordinates(x, y)
+        print(self.posx, self.posy)
     
 def main():
     c.director.director.init(*SZ, caption = CP)
@@ -26,4 +37,3 @@ def main():
     scn = c.scene.Scene(bg, mf, ef)
 
     c.director.director.run(scn)
-
