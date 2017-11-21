5186e179d559f82b185da7a4411780e8bf6afe4f
grishnan <grishnan@gmail.com>
2017-11-21 22:58:02 +0300

    Add new constant. Write virtual_crd_to_cell_crd method. Change field structure in the Field class.

--- const.py
+++ const.py
@@ -1,7 +1,6 @@
 #############################
 ### independent constants ###
 #############################
-
 BG = (244, 180, 180, 255) # background color
 SZ = (1280, 480)          # window's size
 CP = "Sea battle"         # window's caption
@@ -13,11 +12,16 @@ PPIC = "p.png"            # part of a ship picture
 MPIC = "m.png"            # miss picture
 DPIC = "d.png"            # destroy picture
 
-MFPOS = (320, 240)        # my field position
-EFPOS = (960, 240)        # enemy field position
+MFPOS = (320, 240)        # my field position on the background layer
+EFPOS = (960, 240)        # enemy field position on the background layer
 
-SFL = 390                 # field layer size in pixels (don't change this value) 
-SF = 355                  # field size in pixels (don't change this value)
+#########################################################################
+### these values are depended on the field picture, don't change them ###
+#########################################################################
+SFL = 390                 # field layer size in pixels
+SF  = 355                 # field size in pixels
+SC  = 30                  # cell size in pixels
+SB  = 5                   # border size in pixels
 
 ###########################
 ### dependent constants ###
